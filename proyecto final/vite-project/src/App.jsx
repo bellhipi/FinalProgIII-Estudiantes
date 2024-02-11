006155edfd134f00e20794f1d4cd7f6e0683e8d7
bellhipi <bell.hipi@gmail.com>
2024-02-11 20:58:09 -0300

    Scaffolding y librerías

---
+++ proyecto final/vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <h1>Hola pedazo de putos</h1>
+        <p>otra vez yo aca luchando con react</p>      
+    </>
+  )
+}
+
+export default App
