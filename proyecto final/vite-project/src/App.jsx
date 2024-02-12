b7ee5294d584b37ea7e605942d68fab292149d88
bellhipi <bell.hipi@gmail.com>
2024-02-12 15:05:25 -0300

    login front
    modifiqué la navbar y agregué el formulario de logueo

--- proyecto final/vite-project/src/App.jsx
+++ proyecto final/vite-project/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { Button, Checkbox, Form, Input } from 'antd';
 const { Header, Content, Footer } = Layout;
 const items = new Array(2).fill(null).map((_, index) => ({
   key: index + 1,
   label: `nav ${index + 1}`,
 }));
+items[0].label= 'Login';
+items[1].label= 'Register';
+const onFinish = (values) => {
+  console.log('Success:', values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log('Failed:', errorInfo);
+};
 
 const App = () => {
   const {
@@ -12,7 +21,7 @@ const App = () => {
   } = theme.useToken();
   return (
     <Layout>
-      
+
       <Header
         style={{
           display: 'flex',
@@ -43,7 +52,7 @@ const App = () => {
           }}
         >
           <Breadcrumb.Item>Home</Breadcrumb.Item>
-          
+          <Breadcrumb.Item>Login</Breadcrumb.Item>
         </Breadcrumb>
         <div
           style={{
@@ -53,7 +62,72 @@ const App = () => {
             borderRadius: borderRadiusLG,
           }}
         >
-          Content
+          <Form
+            name="basic"
+            labelCol={{
+              span: 8,
+            }}
+            wrapperCol={{
+              span: 16,
+            }}
+            style={{
+              maxWidth: 600,
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your username!',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item
+              name="remember"
+              valuePropName="checked"
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
         </div>
       </Content>
 
